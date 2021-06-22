59ecc501a0e4b1c3bd8e24e02cabc9fb3780a25a
Vivek Thota <vivekthota16@gmail.com>
2021-06-23 00:12:01 +0530

    adding require cuda >11.0

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -28,6 +28,6 @@ if os.path.exists(image_config_file):
 c.Spawner.environment.update(dict(
        NVIDIA_VISIBLE_DEVICES='all',
        NVIDIA_DRIVER_CAPABILITIES='compute,utility',
-       NVIDIA_REQUIRE_CUDA='cuda>=8.0'))
+       NVIDIA_REQUIRE_CUDA='cuda>=11.0'))
 
 c.KubeSpawner.extra_resource_limits = {'nvidia.com/gpu': '1'}
